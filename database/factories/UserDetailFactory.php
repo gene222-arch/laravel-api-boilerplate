f11c39ccbbe88c1d329f87b76264df43b6923479
gene222-arch <genephillip222@gmail.com>
2022-04-14 14:44:29 +0800

    feat: add default faker value

--- database/factories/UserDetailFactory.php
+++ database/factories/UserDetailFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,9 @@ class UserDetailFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'birthed_at' => Carbon::now()->subYears(random_int(1, 5)),
         ];
     }
 }
