4ddf7924f52005aaea0c1a1a78101a20d46176b5
gene222-arch <genephillip222@gmail.com>
2022-05-02 15:09:34 +0800

    feat: implement SoftDelete

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,13 @@ use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
